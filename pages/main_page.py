9cce64b7ca675c6df9f0b50a1cd29de61ee82c30
George Polushkin <polushkin.george@gmail.com>
2024-12-09 00:19:19 +0300

    add locator

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,11 @@
 from pages.base_page import BasePage #импортируем методы класса 
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 class MainPage(BasePage): #наследуем методы класса BasePage для нового класса
 
     def go_to_login_page(self): #ищем и кликаем на ссылки логина 
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self): #пока неправильно ищем ссылку 
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
