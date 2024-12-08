0695b2572387049e984d3826aa4764d60505adf4
George Polushkin <polushkin.george@gmail.com>
2024-12-09 00:12:13 +0300

    add exeption to base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException #импорт эксшепшена
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,4 +6,10 @@ class BasePage():
     
     def open(self):
         self.browser.get(self.url)
-        
+    
+    def is_element_present(self, how, selector): #задаем переменные, которые будут переданы в тесте, в методе приписываем возврат True или False в зависимости от exeption
+        try:
+            self.browser.find_element(how, selector)
+        except (NoSuchElementException):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): #наследуем методы класса BasePage для нового класса
 
-    def go_to_login_page(self):
+    def go_to_login_page(self): #ищем и кликаем на ссылки логина 
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
-    def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+    def should_be_login_link(self): #пока неправильно ищем ссылку 
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
